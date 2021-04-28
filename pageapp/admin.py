44d5baa33b088a79807306bd6c2ba50dda0801e4
Rishav mondal <58592749+rmondal37@users.noreply.github.com>
2021-04-28 20:26:51 +0530

    added database models

--- pageapp/admin.py
+++ pageapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import WebPage, Topic, AccessRecord
 
 # Register your models here.
+
+admin.site.register(WebPage)
+admin.site.register(Topic)
+admin.site.register(AccessRecord)
